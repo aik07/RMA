d605f56173508845f2c08c1a5c012bd24ec7afa3
aik7 <ai.kagwa@gmail.com>
2019-12-09 02:33:13 -0500

    added baseRMA.h and .cpp files

---
+++ src/baseRMA.cpp
@@ -0,0 +1,139 @@
+
+#include "baseRMA.h"
+
+
+namespace base {
+  
+////////////////////// Base class methods ////////////////////////
+
+  // Standard serial read-in code.  Returns true if we can continue, false if
+  // we have to bail out.
+  bool BaseRMA::setup(int& argc, char**& argv) {
+
+    if (!processParameters(argc,argv,min_num_required_args))
+      return false;
+
+    if (plist.size() == 0) {
+        ucout << "Using default values for all solver options" << std::endl;
+    } else {
+      ucout << "User-specified solver options: " << std::endl;
+      plist.write_parameters(ucout);
+      ucout << std::endl;
+    }
+
+    set_parameters(plist,false);
+
+    if ((argc > 0) && !checkParameters(argv[0]))
+      return false;
+
+    if (!setupProblem(argc,argv))
+      return false;
+
+    if (plist.unused() > 0) {
+      ucout << "\nERROR: unused parameters: " << std::endl;
+      plist.write_unused_parameters(ucout);
+      ucout << utilib::Flush;
+      return false;
+    }
+
+    return true;
+
+  }
+
+
+  bool BaseRMA::processParameters(int& argc, char**& argv,
+  				  unsigned int min_num_required_args__) {
+
+    if (argc > 0)
+       solver_name = argv[0];
+    else
+       solver_name = "unknown";
+    if (!parameters_registered) {
+      register_parameters();
+      parameters_registered=true;
+    }
+
+    plist.process_parameters(argc,argv,min_num_required_args__);
+
+    // Set the name of the problem to be the last thing on the command
+    // line. setName will extract the filename root. The setupProblem
+    // method can overwrite this later.
+    if ((argc > 1) && (argv[argc-1] != NULL))
+      setName(argv[1]);
+
+    return true;
+  }
+
+
+  bool BaseRMA::checkParameters(char const* progName) {
+
+    if (help_parameter) {
+      write_usage_info(progName,cout);
+      return false;
+    }
+
+    if (debug_solver_params) {
+      ucout << "---- LPBoost Parameters ----" << endl;
+      write_parameter_values(ucout);
+      ucout << endl << utilib::Flush;
+    }
+
+    return true;
+  }
+
+
+  void BaseRMA::write_usage_info(char const* progName,std::ostream& os) const {
+    writeCommandUsage(progName,os);
+    os << endl;
+    plist.write_registered_parameters(os);
+    os << endl;
+  }
+
+
+  void BaseRMA::writeCommandUsage(char const* progName,std::ostream& os) const {
+    os << "\nUsage: " << progName << " { --parameter=value ... }";
+    if (min_num_required_args == 1)
+      os << " <problem data file>";
+    else if (min_num_required_args == 1)
+      os << " <" << min_num_required_args << " problem data files>";
+    os << endl;
+  }
+
+
+  // This sets the official name of the problem by chewing up the
+  // filename.  It can be overridden.  This version just finds the last
+  // "/" or "\" in the name and removes it and everything before it.
+
+  void BaseRMA::setName(const char* cname) {
+  #if defined (TFLOPS)
+    problemName = cname;
+    int i=problemName.size();
+    while (i >= 0) {
+      if (cname[i] == '/') break;
+      i--;
+    }
+    if (i >= 0)
+       problemName.erase(0,i+1);
+    // TODO: remove the .extension part for this case
+  #else
+    problemName = cname;
+    size_type i = problemName.rfind("/");
+    if (i == string::npos)
+      i = problemName.rfind("\\");
+    if (i != string::npos)
+      problemName.erase(0,i+1);
+
+    size_type n = problemName.length();
+
+    if (n < 4)
+      return;
+
+    string endOfName(problemName,n-4,4);
+    if ((endOfName == ".dat") || (endOfName == ".DAT"))
+      problemName.erase(n-4,n);
+    if ((endOfName == ".data") || (endOfName == ".DATA"))
+        problemName.erase(n-5,n);
+  #endif
+  }
+
+} // end namespacs base
---
+++ src/baseRMA.h
@@ -0,0 +1,78 @@
+/**********************************************************
+ * File name:   baseRMA.h
+ * Author:      Ai Kagawa
+ * Description: a header file for RMA base class
+**********************************************************/
+
+#ifndef BASE_RMA_h
+#define BASE_RMA_h
+
+#include <limits>
+#include <string>
+
+#include "argRMA.h"
+
+#include <pebbl_config.h>
+#include <pebbl/utilib/ParameterSet.h>
+#include <pebbl/utilib/ParameterList.h>
+#include <pebbl/utilib/CommonIO.h>
+#include <pebbl/utilib/memdebug.h>
+#include <pebbl/utilib/seconds.h>
+#include <pebbl/bb/branching.h>
+#include <pebbl/bb/pebblParams.h>
+#include <pebbl/pbb/parPebblParams.h>
+
+using namespace std;
+using namespace utilib;
+using namespace pebbl;
+using namespace arg;
+
+
+namespace base {
+
+class BaseRMA : public ArgRMA,
+                virtual public pebblParams,
+                virtual public parallelPebblParams {
+		  
+public:
+
+  BaseRMA() : parameters_registered(false), min_num_required_args(0) {
+    cout << setprecision(6) << fixed;
+  }
+		  
+  virtual ~BaseRMA() {};
+  
+  /// Setup the solver parameters using command-line information.
+  /// This returns false if there is a problem in the setup, and true
+  /// if the setup appeared to work normally.	
+  bool   setup(int& argc, char**& argv);
+
+  // Parameter-related ethods
+  void   write_usage_info(char const* progName, ostream& os) const;
+  void   writeCommandUsage(char const* progName, ostream& os) const;
+  bool   processParameters(int& argc, char**& argv,
+                          unsigned int min_num_required_args);
+
+  // Register the parameters into a ParameterList object
+  void   register_parameters() { plist.register_parameters(*this); }
+
+  /// Check parameters for setup problems and perform debugging I/O
+  bool   checkParameters(char const* progName = "");
+
+  bool   setupProblem(int argc, char** argv) { true; }
+
+  virtual void setName(const char* cname);
+
+  //private:
+ //////////////////////////////////////////////////////////////////
+  ParameterList plist;
+  bool          parameters_registered;
+  string        problemName;
+  string        solver_name;
+  unsigned int  min_num_required_args;
+
+};
+
+} // end namespace base
+
+#endif
